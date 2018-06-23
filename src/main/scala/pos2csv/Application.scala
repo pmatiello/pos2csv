08068850f3f42d17e645880082c5f61228f13602
Pedro Matiello <pedro@pmatiello.me>
2018-06-23 19:06:59 -0300

    Rename package and classes.

---
+++ src/main/scala/pos2csv/Application.scala
@@ -0,0 +1,7 @@
+package pos2csv
+
+object Application {
+  def main(args: Array[String]): Unit = {
+    println("Hello, world!")
+  }
+}
---
+++ src/test/scala/pos2csv/ApplicationSpec.scala
@@ -0,0 +1,11 @@
+package pos2csv
+
+import org.scalatest._
+
+class ApplicationSpec extends FunSpec with Matchers {
+  describe("ApplicationSpec") {
+    it("works") {
+      1 shouldEqual 1
+    }
+  }
+}
