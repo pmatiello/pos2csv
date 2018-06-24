4890ba61f141693b2e92a1476c13cda1a67c24e2
Pedro Matiello <pedro@pmatiello.me>
2018-06-24 15:11:25 -0300

    Stub application entrypoint.

--- src/main/scala/pos2csv/Application.scala
+++ src/main/scala/pos2csv/Application.scala
@@ -1,7 +1,17 @@
 package pos2csv
 
+import java.io.{InputStream, OutputStream}
+
+class Application(stdin: InputStream, stdout: OutputStream) {
+  def run(): Unit = {
+    io.Source.fromInputStream(stdin).getLines().foreach { str =>
+      stdout.write(s"$str\n".getBytes)
+    }
+  }
+}
+
 object Application {
   def main(args: Array[String]): Unit = {
-    println("Hello, world!")
+    new Application(System.in, System.out).run()
   }
 }
--- src/test/scala/pos2csv/ApplicationSpec.scala
+++ src/test/scala/pos2csv/ApplicationSpec.scala
@@ -1,11 +1,18 @@
 package pos2csv
 
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream}
+
 import org.scalatest._
 
 class ApplicationSpec extends FunSpec with Matchers {
   describe("ApplicationSpec") {
-    it("works") {
-      1 shouldEqual 1
+    it("copies stdin to stdout") {
+      val stdin = new ByteArrayInputStream("abc001\ndef002\n".getBytes)
+      val stdout = new ByteArrayOutputStream
+
+      new Application(stdin, stdout).run()
+
+      stdout.toString shouldEqual "abc001\ndef002\n"
     }
   }
 }
