560927404eedcd89c53ce3de9cd3204a22bdbfd1
Pedro Matiello <pedro@pmatiello.me>
2018-06-26 21:02:51 -0300

    Make IOMapTest clearer.

--- src/test/scala/pos2csv/IOMapTest.scala
+++ src/test/scala/pos2csv/IOMapTest.scala
@@ -7,13 +7,13 @@ import org.scalatest._
 class IOMapTest extends FunSpec with Matchers {
   describe("IOMap") {
     it("applies the given function to each line in stdin and writes the result to stdout") {
-      val fn = (str: String) => s":$str:"
+      val fn = (str: String) => s"fn($str)"
       val in = new ByteArrayInputStream("abc001\ndef002\n".getBytes)
       val out = new ByteArrayOutputStream
 
       new IOMap(fn).map(in, out)
 
-      out.toString shouldEqual ":abc001:\n:def002:\n"
+      out.toString shouldEqual "fn(abc001)\nfn(def002)\n"
     }
   }
 }
