565ba3d5591e628f7a99b938c3df000cafc7d067
Pedro Matiello <pedro@pmatiello.me>
2018-07-14 12:24:15 -0300

    Split stdin lines and write to stdout as CSV.

--- src/main/scala/pos2csv/Application.scala
+++ src/main/scala/pos2csv/Application.scala
@@ -2,12 +2,17 @@ package pos2csv
 
 import java.io.{InputStream, OutputStream}
 
-class Application(stdin: InputStream, stdout: OutputStream) {
-  def run(): Unit = new IOMap(identity).map(stdin, stdout)
+class Application
+(stdin: InputStream, stdout: OutputStream, arguments: List[String]) {
+  private val columSizes = new Arguments(arguments).columnSizes
+
+  private def mapFn(line: String) =
+    new ParsedRow(line, columSizes).cells.mkString(",")
+
+  def run(): Unit = new IOMap(mapFn).map(stdin, stdout)
 }
 
 object Application {
-  def main(args: Array[String]): Unit = {
-    new Application(System.in, System.out).run()
-  }
+  def main(args: Array[String]): Unit =
+    new Application(System.in, System.out, args.toList).run()
 }
--- src/test/scala/pos2csv/ApplicationSpec.scala
+++ src/test/scala/pos2csv/ApplicationSpec.scala
@@ -5,14 +5,15 @@ import java.io.{ByteArrayInputStream, ByteArrayOutputStream}
 import org.scalatest._
 
 class ApplicationSpec extends FunSpec with Matchers {
-  describe("ApplicationSpec") {
+  describe("Application") {
     it("copies stdin to stdout") {
       val stdin = new ByteArrayInputStream("abc001\ndef002\n".getBytes)
       val stdout = new ByteArrayOutputStream
+      val arguments = "1,2,3" :: Nil
 
-      new Application(stdin, stdout).run()
+      new Application(stdin, stdout, arguments).run()
 
-      stdout.toString shouldEqual "abc001\ndef002\n"
+      stdout.toString shouldEqual "a,bc,001\nd,ef,002\n"
     }
   }
 }
